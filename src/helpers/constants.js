23409a12bd919cfeb53854c02d7315bae27812db
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-02-07 16:39:34 +0300

    update env - prep for prod

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,11 +1,11 @@
 import HmacMD5 from 'crypto-js/hmac-md5'
 import EncBase64 from 'crypto-js/enc-base64'
 
-export const url ='https://sandbox-authservice.priaid.ch/login'
-export const urlHealth = 'https://sandbox-healthservice.priaid.ch'
+export const url ='https://authservice.priaid.ch/login'
+export const urlHealth = 'https://healthservice.priaid.ch'
 
-export const apiKey = process.env.REACT_APP_API_KEY
-export const secretKey = process.env.REACT_APP_SECRET_KEY
+export const apiKey = process.env.REACT_APP_API_KEY_PROD
+export const secretKey = process.env.REACT_APP_SECRET_KEY_PROD
 
 export const computedHash = HmacMD5(url, secretKey)
 export const computedHashString = computedHash.toString(EncBase64)
