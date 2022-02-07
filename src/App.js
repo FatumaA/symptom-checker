8b0d4f855ddc1bb3e40a0f510e307965426a96f3
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-02-07 14:35:00 +0300

    refine loading state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
-import './App.css';
+import './App.css'
 import { useState, useEffect } from 'react'
-import { symptomsURLPath } from './helpers/axios';
-import Header from './components/Header';
-import DisplaySymptoms from './components/DisplaySymptoms';
+import { symptomsURLPath, setToken, storedToken } from './helpers/axios'
+import Header from './components/Header'
+import DisplaySymptoms from './components/DisplaySymptoms'
 
 
-const App= () => {
-
+const App = () => {
     const [symptoms, setSymptoms] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+
     
+    const isTokenAbsent = storedToken === null 
+                                || storedToken === ''
+                                || storedToken === undefined
+    
+
     const getSymptoms = async () => { 
         try{
-         const result = await symptomsURLPath
-         console.log(result.data)
-         debugger
-         setSymptoms(result.data)
-         setIsLoading(false)
-         debugger
+            const result = await symptomsURLPath
+            console.log(result.data)
+            setSymptoms(result.data)
+            setIsLoading(false)
         } 
-        catch{}
+        catch(e){
+            console.log('error', e)
+            if(e.response.status === 400) {
+                window.localStorage.clear()
+                setToken()     
+            }
+            setIsError(true)
+        }
      }
 
-    useEffect (() => getSymptoms(),[])
+    useEffect (() => { 
+        if(isTokenAbsent){
+            setToken()
+            getSymptoms()
+        } else {
+            getSymptoms() 
+        } 
+    }, [isTokenAbsent])
         
     return (
         <>
         <div className='top-style'>
           <Header className='header-style'/>
           <DisplaySymptoms className='second-section-style' 
-            isLoading={isLoading} symptoms={symptoms} />
+            isLoading={isLoading} isError={isError} symptoms={symptoms} />
         </div>
         </>
     )
--- src/components/DisplaySymptoms.js
+++ src/components/DisplaySymptoms.js
@@ -1,9 +1,10 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Select from 'react-select'
-import DisplayDiagnosis from './DisplayDiagnosis';
+
+import DisplayDiagnosis from './DisplayDiagnosis'
 
 
-const DisplaySymptoms = ({symptoms, isLoading}) => {
+const DisplaySymptoms = ({symptoms, isLoading, isError}) => {
   
   const [selectedSymptoms, setSelectedSymptoms] = useState([])
   const [selectedGender, setSelectedGender] = useState('')
@@ -12,7 +13,6 @@ const DisplaySymptoms = ({symptoms, isLoading}) => {
   const [openModal, setopenModal] = useState(false)
   
   
-  // const {reset, control} = useForm()
   
   const handleChange = (e)=>{
           setSelectedSymptoms(Array.isArray(e)? e.map(x => x.value): [] );
@@ -20,12 +20,12 @@ const DisplaySymptoms = ({symptoms, isLoading}) => {
    
   
   const handleChangeGender = (selectedGender) => {
-    setSelectedGender(selectedGender)
-    }
+          setSelectedGender(selectedGender)
+        }
   
   const handleChangeYob= (selectedYob) => {
-      setSelectedYob(selectedYob)
-    }
+          setSelectedYob(selectedYob)
+        }
    
   const options= symptoms.map(symptom => ({
     'value': symptom.ID,
@@ -37,14 +37,13 @@ const DisplaySymptoms = ({symptoms, isLoading}) => {
     {'value': 'female','label': 'Female'}
   ]
   
-  const years=[ 1930,1931,1932,1933,1934,1935,1936,1937,1938,1939,1940,
-               1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950,
+  const years=[1940,1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950,
                1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 
                1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971,
                1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981,
                1982,1983, 1984, 1985, 1986, 1987, 1988,1989, 1990, 1991, 1992, 1993, 1994, 1995,
                1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
-               2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+               2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
  
     const yob= years.map((year, index) => ({ 'value': year, 'label': year }))
 
@@ -65,17 +64,18 @@ const DisplaySymptoms = ({symptoms, isLoading}) => {
     }
 
 
-    const handleReset = () => { 
-      setSelectedSymptoms([]);
-      setSelectedGender(null);
-      setSelectedYob(null);  
+  const handleReset = () => { 
+    setSelectedSymptoms([]);
+    setSelectedGender(null);
+    setSelectedYob(null);  
   }
-  
+ 
   return (
-    isLoading ?  <div className='error'> App unavailable, please try later</div> :
+    isLoading && !isError ? <div className='loading'> <div></div> </div>  :
+    isError && !isLoading ?  <div className='error'> App unavailable, please try later</div> :
     <>
     <div className='form-container'>
-    <form  onSubmit ={handleSubmit}>
+    <form  onSubmit = { handleSubmit }>
 
     <label htmlFor='Symptoms-input'>Please select your symptoms</label>
         <Select 
@@ -94,7 +94,6 @@ const DisplaySymptoms = ({symptoms, isLoading}) => {
          <Select 
           className='holds-results'
           placeholder='Select Gender'
-          // value={gender.find(selectedGender => selectedGender.value === selectedGender.value)}
           value = {selectedGender}
           onChange={handleChangeGender}
           options={gender}
@@ -105,7 +104,6 @@ const DisplaySymptoms = ({symptoms, isLoading}) => {
           <Select 
           className='holds-results'
           placeholder='Select Year'
-          // value={yob.find(selectedYob => selectedYob.label === selectedYob.value)}
           value = {selectedYob}
           onChange={handleChangeYob}
           options={yob}
@@ -113,9 +111,7 @@ const DisplaySymptoms = ({symptoms, isLoading}) => {
           defaultValue=''
           /> 
     
-    <button type='submit' className='btn-primary' 
-    // onClick={()=>setopenModal(true)}
-    > Submit </button>
+    <button type='submit' className='btn-primary'> Submit </button>
 
     </form>
 
