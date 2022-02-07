f7325fff2b924bc21d6770d978b8b4be51b73fa1
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-02-07 14:35:41 +0300

    cleaned out axios helper

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,53 +2,24 @@ import axios from 'axios'
 import { urlHealth, apiKey, computedHashString, url } from './constants';
 
 const data = {}
-const headers = { 'Authorization': `Bearer ${apiKey}:${computedHashString}`,
-'Access-Control-Allow-Origin': "*",
-' Access-Control-Allow-Methods': "POST, GET, OPTIONS, DELETE, PUT",
- 'Access-Control-Allow-Headers': "Content-Type"
-
-}
-
-
-axios.interceptors.request.use((config) => {
-    // Do something before request is sent
-    // if(config.method === 'get'){
-            debugger
-            config.headers.token = window.localStorage.getItem('authToken')   
-        //  }
-    return config;
-  }, (error) => {
-    return Promise.reject(error);
-  });
-
-  axios.interceptors.response.use((response) => {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  }, (error) => {
-      debugger
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-        window.localStorage.clear('authToken')
-        debugger
-        setToken()
-    return Promise.reject(error);
-  });
-
+const headers = { 'Authorization': `Bearer ${apiKey}:${computedHashString}` }
 
 let token;
 
-export const setToken = () => { 
-    debugger
-    axios.post(url, data, { headers })
-         .then(response => { 
-             debugger; 
-             token = { authToken: response.data.Token }; 
-             window.localStorage.setItem('authToken', token.authToken)
-            })
-         .catch(error => console.log('error:', error))
-    }
-// https://cors-anywhere.herokuapp.com/
-export const symptomsURL = `${urlHealth}/symptoms?token=${window.localStorage.getItem('authToken')}&language=en-gb`
-export const symptomsURLPath = axios(symptomsURL)   
 
+// gets and stores the token from the apimedic api
+export const setToken = () => { 
+            axios.post(url, data, { headers })
+            .then(response => { 
+                token = response.data.Token; 
+                window.localStorage.setItem('authToken', token)
+                window.location.reload()
+               })
+            .catch(error => console.log('error:', error))
+    }
+
+
+export const storedToken = window.localStorage.getItem('authToken');
+
+const symptomsURL = `${urlHealth}/symptoms?token=${storedToken}&language=en-gb`
+export const symptomsURLPath = axios(symptomsURL)
